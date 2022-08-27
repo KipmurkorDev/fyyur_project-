46e3ded915d6c92bb7545a34f3e5e9eabf2c5267
EMMANUEL KIPMURKOR <56157281+KipmurkorDev@users.noreply.github.com>
2022-08-27 12:14:39 +0300

    Delete sample-react-portfolio directory

--- sample-react-portfolio/src/assets/social-icons/index.js
+++
@@ -1,6 +0,0 @@
-export default {
-  facebook: require(`./facebook.png`).default,
-  instagram: require(`./instagram.png`).default,
-  linkedin: require(`./linkedin.png`).default,
-  youtube: require(`./youtube.png`).default,
-};
